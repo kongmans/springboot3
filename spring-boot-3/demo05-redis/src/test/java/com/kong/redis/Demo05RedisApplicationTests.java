38ebea50444dbba296b3fce2aca7df5d8f3af243
kongman <kongman@petalmail.com>
2023-11-09 17:28:52 +0800

    second

--- spring-boot-3/demo05-redis/src/test/java/com/kong/redis/Demo05RedisApplicationTests.java
+++ spring-boot-3/demo05-redis/src/test/java/com/kong/redis/Demo05RedisApplicationTests.java
@@ -3,8 +3,10 @@ package com.kong.redis;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.kong.redis.pojo.User;
+import com.kong.redis.utils.RedisUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.redis.connection.RedisConnection;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -13,18 +15,33 @@ import org.springframework.data.redis.core.RedisTemplate;
 class Demo05RedisApplicationTests {
 	@Autowired
 	private RedisTemplate redisTemplate;
+
+    @Autowired
+    RedisUtil redisUtil;
+
 	@Test
 	void contextLoads() {
-//		RedisConnection connection = redisTemplate.getRequiredConnectionFactory().getConnection();
-//		connection.flushDb();
+		RedisConnection connection = redisTemplate.getRequiredConnectionFactory().getConnection();
+        System.out.println(connection);
 
-		redisTemplate.opsForValue().set("name","张三");
-		System.out.println(redisTemplate.opsForValue().get("name"));
 
 	}
 
+
     @Test
-    public void test() throws JsonProcessingException {
+    public void test3(){
+        redisUtil.set("user2","lisi");
+        System.out.println(redisTemplate.opsForValue().get("user2"));
+    }
+    @Test
+    public void test1() {
+        redisTemplate.opsForValue().set("name","张三");
+        System.out.println(redisTemplate.opsForValue().get("name"));
+
+    }
+
+    @Test
+    public void test2() throws JsonProcessingException {
         User user1 = new User("kongman", 12);
         String jsonUser = new ObjectMapper().writeValueAsString(user1);
         redisTemplate.opsForValue().set("user",jsonUser);
